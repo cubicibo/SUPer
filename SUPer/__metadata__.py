d3a0660c04d35288cb748d32436fc35e1f453736
cubicibo <55701024+cubicibo@users.noreply.github.com>
2024-02-10 09:52:09 +0100

    Push version.

--- SUPer/__metadata__.py
+++ SUPer/__metadata__.py
@@ -20,7 +20,7 @@ along with SUPer.  If not, see <http://www.gnu.org/licenses/>.
 
 __MAJOR = 0
 __MINOR = 2
-__REVISION = 3
+__REVISION = 4
 
 __name__    = "SUPer"
 __version__ = '.'.join(map(str, [__MAJOR, __MINOR, __REVISION]))
