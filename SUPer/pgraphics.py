daa10173eec001ceb436ed25a5995df8a597f984
cubicibo <55701024+cubicibo@users.noreply.github.com>
2024-05-24 17:55:26 +0200

    pgraphics: use Brule as run-length coder/decoder.

--- SUPer/pgraphics.py
+++ SUPer/pgraphics.py
@@ -27,87 +27,8 @@ from .segments import ODS, PDS
 from .utils import Shape, Box, MPEGTS_FREQ
 
 from dataclasses import dataclass
-#%%
-try:
-    #If numba is available, provide compiled functions for the encoder/decoder
-    from numba import njit
+from brule import Brule
 
-    @njit(fastmath=True)
-    def njit_encode_rle(bitmap: npt.NDArray[np.uint8]) -> list[np.uint8]:
-        """
-        Encode a 2D map using the RLE defined in 'US 7912305 B1' patent.
-        :param bitmap:    Palette mapped image to encode (2d array)
-        :return:          Encoded data (vector)
-        """
-        i, j = 0, 0
-        rle_data = [np.uint8(x) for x in range(0)]
-
-        height, width = bitmap.shape
-        assert width <= 16383, "Bitmap too large."
-
-        while i < height:
-            color = bitmap[i, j]
-            prev_j = j
-            while (j := j+1) < width and bitmap[i, j] == color: pass
-
-            dist = j - prev_j
-            if color == 0:
-                if dist > 63:
-                    rle_data += [0x00, 0x40 | ((dist >> 8) & 0x3F), dist & 0xFF]
-                else:
-                    rle_data += [0x00, dist & 0x3F]
-            else:
-                if dist > 63:
-                    rle_data += [0x00, 0xC0 | ((dist >> 8) & 0x3F), dist & 0xFF, color]
-                elif dist > 2:
-                    rle_data += [0x00, 0x80 | (dist & 0x3F), color]
-                else:
-                    rle_data += [color] * dist
-            if j == width:
-                j = 0
-                i += 1
-                rle_data += [0x00, 0x00]
-        return rle_data
-
-    @njit(fastmath=True)
-    def njit_decode_rle(rle_data: Union[list[np.uint8], npt.NDArray[np.uint8], bytes], width: int, height: int, check_rle: bool = False) -> npt.NDArray[np.uint8]:
-        i, j, k = 0, -1, -2
-        # RLE is terminated by new line command ([0x00, 0x00]), we can ignore it.
-        len_rle = len(rle_data) - 2
-        bitmap = np.zeros((height, width), np.uint8)
-        line_length = 0
-
-        while k < len_rle:
-            if i % width == 0:
-                if line_length > 0:
-                    assert 0 == rle_data[k] == rle_data[k+1], "RLE line terminator not found at given width."
-                    assert not check_rle or line_length <= width, "Illegal RLE line width."
-                    line_length = 0
-                i = 0
-                j += 1
-                k += 2
-            k_start = k
-            byte = rle_data[k]
-            if byte == 0:
-                byte = rle_data[(k:=k+1)]
-                if byte & 0x40:
-                    length = ((byte & 0x3F) << 8) | rle_data[(k:=k+1)]
-                else:
-                    length = byte & 0x3F
-                if byte & 0x80:
-                    bitmap[j, i:(i:=i+length)] = rle_data[(k:=k+1)]
-                else:
-                    bitmap[j, i:(i:=i+length)] = 0
-            else:
-                bitmap[j, i:(i:=i+1)] = byte
-            k+=1
-            line_length += (k - k_start)
-        return bitmap
-
-except ModuleNotFoundError:
-    njit_decode_rle = None
-    njit_encode_rle = None
-####
 #%%
 class PGraphics:
     @classmethod
@@ -126,38 +47,10 @@ class PGraphics:
         :param bitmap:    Palette mapped image to encode (2d array)
         :return:          Encoded data (vector)
         """
-        if njit_encode_rle is not None:
-            return njit_encode_rle(bitmap)
-
-        rle_data = []
-        i, j = 0, 0
-
-        height, width = bitmap.shape
-        assert width <= 16383, "Bitmap too large."
-
-        while i < height:
-            color = bitmap[i, j]
-            prev_j = j
-            while (j := j+1) < width and bitmap[i, j] == color: pass
-
-            dist = j - prev_j
-            if color == 0:
-                if dist > 63:
-                    rle_data += [0x00, 0x40 | ((dist >> 8) & 0x3F), dist & 0xFF]
-                else:
-                    rle_data += [0x00, dist & 0x3F]
-            else:
-                if dist > 63:
-                    rle_data += [0x00, 0xC0 | ((dist >> 8) & 0x3F), dist & 0xFF, color]
-                elif dist > 2:
-                    rle_data += [0x00, 0x80 | (dist & 0x3F), color]
-                else:
-                    rle_data += [color] * dist
-            if j == width:
-                j = 0
-                i += 1
-                rle_data += [0x00, 0x00]
-        return bytes(rle_data)
+        rle_data = Brule.encode(bitmap)
+        if not isinstance(rle_data, (bytes, bytearray)):
+            return bytes(rle_data)
+        return rle_data
 
     @staticmethod
     def decode_rle(data: Union[bytes, bytearray, ODS, list[ODS]],
@@ -187,40 +80,8 @@ class PGraphics:
             else:
                 data = bytearray(data)
 
-        if njit_decode_rle and width and height:
-            return njit_decode_rle(np.asarray(data, dtype=np.uint8), width, height, check_rle)
-
-        k = 0
-        len_data = len(data)
-        bitmap = []
-        line = []
-        rle_line_length = 0
-
-        while k < len_data:
-            k_start = k
-            byte = data[k]
-            if byte == 0:
-                byte = data[(k:=k+1)]
-                if byte == 0:
-                    assert not check_rle or width and rle_line_length <= width
-                    bitmap.append(line)
-                    line = []
-                    rle_line_length = 0
-                    k += 1
-                    continue
-                if byte & 0x40:
-                    length = ((byte & 0x3F) << 8) | data[(k:=k+1)]
-                else:
-                    length = byte & 0x3F
-                if byte & 0x80:
-                    line += [data[(k:=k+1)]]*length
-                else:
-                    line += [0]*length
-            else:
-                line.append(byte)
-            k+=1
-            rle_line_length += (k - k_start)
-        return np.asarray(bitmap, np.uint8)
+        assert isinstance(data, (bytes, bytearray))
+        return Brule.decode(data, width, height, check_rle)
 
     @staticmethod
     def show(l_ods: Union[ODS, list[ODS]],
