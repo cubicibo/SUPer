ccbeb7b2a12f5fed32bbfeb056a951fddc6669a2
cubicibo <55701024+cubicibo@users.noreply.github.com>
2024-07-25 12:47:06 +0200

    test: remove layout 3rd dimension in test cases.

--- tests/test_r2.py
+++ tests/test_r2.py
@@ -32,9 +32,9 @@ def test_pad_wds():
     box = ge.pad_box()
     assert box == input_box
 
-    gs_orig = np.zeros((1, box.dy, box.dx), np.uint8)
-    gs_orig[0, 0, 0] = 1
-    gs_orig[0,-1,-1] = 1
+    gs_orig = np.zeros((box.dy, box.dx), np.uint8)
+    gs_orig[ 0, 0] = 1
+    gs_orig[-1,-1] = 1
     windows = ge.find_layout(gs_orig)
     assert len(windows) == 2
     np_mask = np.ones((box.dy, box.dx), np.bool_)
@@ -42,7 +42,7 @@ def test_pad_wds():
         np_mask[wd.y:wd.y2, wd.x:wd.x2] = False
         assert Box.union(wd, box).overlap_with(container) == 1.0
         assert wd.dx == 8 and wd.dy == 8
-    assert not np.any(gs_orig[0, np_mask]), windows
+    assert not np.any(gs_orig[np_mask]), windows
 
 def test_merge_wds():
     container = Box(0, 480, 0, 720)
@@ -51,9 +51,9 @@ def test_merge_wds():
     ge = GroupingEngine(box, container, 2)
     assert ge.pad_box() == box
 
-    gs_orig = np.zeros((1, box.dy, box.dx), np.uint8)
-    gs_orig[0, 0, 0] = 1
-    gs_orig[0, 9, 9] = 1
+    gs_orig = np.zeros((box.dy, box.dx), np.uint8)
+    gs_orig[0, 0] = 1
+    gs_orig[9, 9] = 1
     windows = ge.find_layout(gs_orig)
     assert len(windows) == 1, windows
     np_mask = np.ones((box.dy, box.dx), np.bool_)
@@ -62,7 +62,7 @@ def test_merge_wds():
         np_mask[wd.y:wd.y2, wd.x:wd.x2] = False
         assert Box.union(wd, box).overlap_with(box) == 1.0
         assert wd.dx >= 8 and wd.dy >= 8
-    assert not np.any(gs_orig[0, np_mask]), windows
+    assert not np.any(gs_orig[np_mask]), windows
 
 def test_merge_tick_overhead():
     container = Box(0, 480, 0, 720)
@@ -71,16 +71,16 @@ def test_merge_tick_overhead():
     ge = GroupingEngine(box, container, 2)
     assert ge.pad_box() == box
 
-    gs_orig = np.zeros((1, box.dy, box.dx), np.uint8)
-    gs_orig[0, 0, 0] = 1
-    gs_orig[0, -1, 0] = 1
+    gs_orig = np.zeros((box.dy, box.dx), np.uint8)
+    gs_orig[0, 0] = 1
+    gs_orig[-1,0] = 1
     windows = ge.find_layout(gs_orig)
     assert len(windows) == 1, windows
     np_mask = np.ones((box.dy, box.dx), np.bool_)
     for wd in windows:
         assert Box.union(wd, box).overlap_with(box) == 1.0
         np_mask[wd.y:wd.y2, wd.x:wd.x2] = False
-    assert not np.any(gs_orig[0, np_mask]), (windows, np.argwhere(gs_orig[0, :, :] == 1))
+    assert not np.any(gs_orig[np_mask]), (windows, np.argwhere(gs_orig[:, :] == 1))
 
 def test_split_tick_overhead():
     container = Box(0, 480, 0, 720)
@@ -89,9 +89,9 @@ def test_split_tick_overhead():
     ge = GroupingEngine(box, container, 2)
     assert ge.pad_box() == box
 
-    gs_orig = np.zeros((1, box.dy, box.dx), np.uint8)
-    gs_orig[0, 0, :] = 1
-    gs_orig[0, 8, 0] = 1
+    gs_orig = np.zeros((box.dy, box.dx), np.uint8)
+    gs_orig[0, :] = 1
+    gs_orig[8, 0] = 1
     windows = ge.find_layout(gs_orig)
     assert len(windows) == 2, windows
     np_mask = np.ones((box.dy, box.dx), np.bool_)
@@ -99,7 +99,7 @@ def test_split_tick_overhead():
         np_mask[wd.y:wd.y2, wd.x:wd.x2] = False
         assert Box.union(wd, box).overlap_with(box) == 1.0
         assert Box(wd.y+box.y, wd.dy, wd.x+box.x, wd.dx).overlap_with(container) == 1.0
-    assert not np.any(gs_orig[0, np_mask])
+    assert not np.any(gs_orig[np_mask])
 
 def test_pad_marging_box():
     container = Box(0, 480, 0, 720)
@@ -108,25 +108,25 @@ def test_pad_marging_box():
     ge = GroupingEngine(box, container, 2)
     assert ge.pad_box() == box
 
-    gs_orig = np.zeros((1, box.dy, box.dx), np.uint8)
-    gs_orig[0, 8, 0] = 1
-    gs_orig[0, 10:13, :] = 1
+    gs_orig = np.zeros((box.dy, box.dx), np.uint8)
+    gs_orig[8, 0] = 1
+    gs_orig[10:13, :] = 1
     windows = ge.find_layout(gs_orig)
 
     np_mask = np.ones((box.dy, box.dx), np.bool_)
     for wd in windows:
         np_mask[wd.y:wd.y2, wd.x:wd.x2] = False
         assert Box(wd.y+box.y, wd.dy, wd.x+box.x, wd.dx).overlap_with(container) == 1.0, windows
-    assert not np.any(gs_orig[0, np_mask])
+    assert not np.any(gs_orig[np_mask])
 
 def test_single_window():
     container = Box(0, 480, 0, 720)
     box = Box(100, 100, 100, 100)
     ge = GroupingEngine(box, container, 1)
 
-    gs_orig = np.zeros((1, box.dy, box.dx), np.uint8)
-    gs_orig[0, 0, 0] = 1
-    gs_orig[0, -1, -1] = 1
+    gs_orig = np.zeros((box.dy, box.dx), np.uint8)
+    gs_orig[ 0, 0] = 1
+    gs_orig[-1,-1] = 1
     assert len(ge.find_layout(gs_orig)) == 1
 
 def test_pad_centered_box():
